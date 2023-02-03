744f4cf5f89b71e006804a5a33330477bd354a74
teng <tengwenjie0221@yahoo.co.jp>
2023-02-03 15:55:29 +0900

    add delete age

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <h3>名前：{{ user.name }}</h3>
-  <h3>年齢：{{ user.age }}</h3>
+  <h3 v-show="user.age">年齢：{{ user.age }}</h3>
   <h3>仕事：{{ user.job }}</h3>
   <h3>趣味：{{ user.hobby }}</h3>
   <h3 v-show="user.sex">性別：{{ user.sex }}</h3>
   <h3 v-show="user.company">会社：{{ user.company }}</h3>
-  <button @click="changeInfo">change name</button>
-  <button @click="addGanderAndCompany">add gander</button>
+  <br />
+  <button @click="changeInfo">change name</button><br />
+  <button @click="addGanderAndCompany">add gander</button><br />
+  <button @click="deleteAge">delete age</button>
 </template>
 
 <script>
@@ -28,10 +30,14 @@ export default {
       user.sex = '男';
       user.company = 'pactera';
     }
+    function deleteAge() {
+      delete user.age;
+    }
     return {
       user,
       changeInfo,
       addGanderAndCompany,
+      deleteAge,
     };
   },
 };
