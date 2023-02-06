7040f8d4f514fdf91f26b26feb681ff35b09ae28
teng <tengwenjie0221@yahoo.co.jp>
2023-02-06 16:48:06 +0900

    Vue3 computed

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,11 @@
 <template>
-  <h3>名前：{{ user.name }}</h3>
-  <h3 v-show="user.age">年齢：{{ user.age }}</h3>
-  <h3>仕事：{{ user.job }}</h3>
-  <h3>趣味：{{ user.hobby }}</h3>
-  <h3 v-show="user.sex">性別：{{ user.sex }}</h3>
-  <h3 v-show="user.company">会社：{{ user.company }}</h3>
-  <button @click="changeInfo">change name</button>
-  <button @click="addGanderAndCompany">add gander</button>
+  <DemoItem />
 </template>
 
 <script>
-import { reactive } from 'vue';
+import DemoItem from './components/DemoItem';
 export default {
   name: 'App',
-  setup() {
-    let user = reactive({
-      name: 'tom',
-      age: 18,
-      job: 'Web Enageer',
-      hobby: ['basketball', 'football', 'pingpang'],
-    });
-    function changeInfo() {
-      user.name = 'David';
-      (user.age = 20), (user.job = 'Java Enageer'), (user.hobby = ['vido Game', 'Movie', 'music']);
-    }
-    function addGanderAndCompany() {
-      user.sex = '男';
-      user.company = 'pactera';
-    }
-    function deleteAge() {
-      delete user.age;
-    }
-    function tobe() {}
-    return {
-      user,
-      changeInfo,
-      addGanderAndCompany,
-      deleteAge,
-      tobe,
-    };
-  },
+  components: { DemoItem },
 };
 </script>
---
+++ src/components/DemoItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <h2>profile</h2>
+  first name:<input type="text" v-model="person.firstName" /><br />
+  last name :<input type="text" v-model="person.lastName" />
+  <hr />
+  <h3>{{ person.fullName }}</h3>
+  <input type="text" v-model="person.fullName" />
+</template>
+<script>
+import { reactive, computed } from 'vue';
+export default {
+  name: 'DemoItem',
+  props: ['msg', 'code'],
+  emits: ['hello'],
+  setup() {
+    let person = reactive({
+      firstName: 'Zhang',
+      lastName: 'san',
+    });
+    // person.fullName = computed(() => {
+    //   return person.firstName + '-' + person.lastName;
+    // });
+    person.fullName = computed({
+      get() {
+        return person.firstName + '-' + person.lastName;
+      },
+      set(value) {
+        const nameArr = value.split('-');
+        person.firstName = nameArr[0];
+        person.lastName = nameArr[1];
+      },
+    });
+    return { person };
+  },
+};
+</script>
