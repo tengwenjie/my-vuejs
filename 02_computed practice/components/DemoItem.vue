c12dae6f7e6af52377faff534acabb0a6d676474
teng <tengwenjie0221@yahoo.co.jp>
2023-02-08 10:19:01 +0900

    computed practice

---
+++ 02_computed practice/components/DemoItem.vue
@@ -0,0 +1,34 @@
+<template>
+  first name:<input type="text" v-model="person.firstName"/><br>
+  last name:<input type="text" v-model="person.lastName"/><br>
+  <hr/>
+  <h3>full name:{{ person.fullName }}</h3>
+  full name:<input type="text" v-model="person.fullName"/>
+  
+</template>
+<script>
+import { reactive,computed } from 'vue';
+export default {
+  name: 'DemoItem',
+  setup() {
+    let person = reactive({
+      firstName:'zhang',
+      lastName:'san',
+      fullName:''
+    });
+    
+    person.fullName = computed({
+      get(){
+        return person.firstName +" "+ person.lastName
+      },
+      set(value){
+        const nameArr= value.split(' ')
+        person.firstName = nameArr[0]
+        person.lastName=nameArr[1]
+      },
+    })
+
+    return { person};
+  },
+};
+</script>
