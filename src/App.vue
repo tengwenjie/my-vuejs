004f77bcb352ed8df7fbf1c5ba08ea764bcbf206
teng <tengwenjie0221@yahoo.co.jp>
2023-02-09 13:29:03 +0900

    toRef toRefs practice

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <template>
   <button @click="showDemoItem = !showDemoItem">show or close</button>
   <DemoItem v-if="showDemoItem" />
-  <hr />
-  <TestItem />
 </template>
 
 <script>
 import DemoItem from './components/DemoItem';
-import TestItem from './components/TestItem';
 import { ref } from 'vue';
 export default {
   name: 'App',
-  components: { DemoItem, TestItem },
+  components: { DemoItem },
   setup() {
     let showDemoItem = ref(true);
     return { showDemoItem };
--- src/components/DemoItem.vue
+++ src/components/DemoItem.vue
@@ -1,15 +1,28 @@
 <template>
-  <h3>DemoItem</h3>
-  <h3>coordinate:</h3>
-  <span>X:{{ point.x }},</span><span>Y:{{ point.y }}</span>
+  <h3>{{ name }}</h3>
+  <h3>{{ age }}</h3>
+  <h3>{{ job.j1.salary }}</h3>
+  <button @click="name += '~'">name+</button>
+  <button @click="age++">age+</button>
+  <button @click="job.j1.salary++">salary+</button>
+  <hr />
+  <h3>{{ person }}</h3>
 </template>
 <script>
-import usePoint from '../hooks/usePoint';
+import { reactive, toRefs } from 'vue';
 export default {
   name: 'DemoItem',
   setup() {
-    let point = usePoint();
-    return { point };
+    const person = reactive({
+      name: 'Zhang San',
+      age: 18,
+      job: {
+        j1: {
+          salary: 20,
+        },
+      },
+    });
+    return { ...toRefs(person), person };
   },
 };
 </script>
